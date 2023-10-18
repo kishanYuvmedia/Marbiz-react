e5324732a23b1a260c1dd48611252a0e95788237
It1yuvmedia <it1@yuvmedia.com>
2023-10-18 16:18:15 +0530

    update 18-10 brand created

--- marbiz-frontend/src/Page/Brand/BrandSignup.jsx
+++ marbiz-frontend/src/Page/Brand/BrandSignup.jsx
@@ -1,28 +1,92 @@
-import React, { useState } from "react";
-import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import Select from "react-select";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
 import Swal from "sweetalert2";
 import HeroBgGradient from "../../Components/HeroBgGradient";
 import config from "../../constants/config";
+import { getPublicList, createMtUsers } from "../../services/api/api-service";
 const BrandSignup = () => {
+  const navigate = useNavigate();
+  document.title = "Create Account | Marbiz";
   const responseGoogle = (response) => {
     console.log(response);
   };
-
   const [validated, setValidated] = useState(false);
+  const [category, setCategory] = useState([]);
+  const [platform, setPlatform] = useState([]);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    brandName: "",
+    businessEmail: "",
+    password: "",
+    howDidYouHear: "",
+    interestedCategories: [],
+  });
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    setValidated(true);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
+  const handleCategoryChange = (selectedOptions) => {
+    const categoryValues = selectedOptions.map((option) => option.value);
+    setFormData({
+      ...formData,
+      interestedCategories: categoryValues,
+    });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      // Access the form data here
+      console.log(formData);
+      const data = [];
+      data.push({
+        phoneVerified: false,
+        isTermsAgreed: false,
+        contactName: formData.fullName,
+        userType: "Business",
+        country: "IN",
+        status: "A",
+        realm: formData.fullName,
+        username: formData.businessEmail,
+        password: formData.password,
+        email: formData.businessEmail,
+        howDidYouHear: formData.howDidYouHear,
+        interestedCategories: formData.interestedCategories,
+        emailVerified: true,
+      });
+      createMtUsers(data).then((result) => {
+        if (result) {
+          Swal.fire(
+            "Congratulations",
+            "Your Marbiz accunt is created",
+            "success"
+          );
+          setValidated(true);
+          navigate(`/login`, {
+            replace: true,
+          });
+        }
+      });
+    }
+  };
+  useEffect(() => {
+    getPublicList("Category").then((result) => {
+      setCategory(result);
+    });
+    getPublicList("Platform").then((result) => {
+      setPlatform(result);
+    });
+  }, []);
   return (
     <div>
       <Container
@@ -50,7 +114,6 @@ const BrandSignup = () => {
                 }}
               />
             </div>
-
             <div className="py-2 d-grid align-content-center">
               <h2 className="text-divider">
                 <span className="">or</span>
@@ -59,64 +122,81 @@ const BrandSignup = () => {
 
             {/* signup form */}
             <div className="inquiry-form signupForm rounded-3 p-3">
-              <Form
+              <form
                 className="m-3 pb-3"
                 noValidate
                 validated={validated}
                 onSubmit={handleSubmit}
               >
-                {/* Full Name */}
                 <Form.Group className="mb-3" controlId="fullName">
                   <Form.Control
                     required
                     className="dark-bg"
                     type="text"
                     placeholder="Full Name"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={handleChange}
                   />
+                  {/* ... (Feedback and validation message) ... */}
                 </Form.Group>
 
-                {/* Brand Name */}
                 <Form.Group className="mb-3" controlId="brandName">
                   <Form.Control
                     required
                     className="dark-bg"
                     type="text"
                     placeholder="Brand Name"
+                    name="brandName"
+                    value={formData.brandName}
+                    onChange={handleChange}
                   />
+                  {/* ... (Feedback and validation message) ... */}
                 </Form.Group>
 
-                {/* Business Email */}
                 <Form.Group className="mb-3" controlId="businessEmail">
                   <Form.Control
                     required
                     className="dark-bg"
                     type="email"
                     placeholder="Business Email"
+                    name="businessEmail"
+                    value={formData.businessEmail}
+                    onChange={handleChange}
                   />
+                  {/* ... (Feedback and validation message) ... */}
                 </Form.Group>
 
-                {/* Password */}
                 <Form.Group className="mb-3" controlId="password">
                   <Form.Control
                     required
                     className="dark-bg"
                     type="password"
                     placeholder="Password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
                   />
+                  {/* ... (Feedback and validation message) ... */}
                 </Form.Group>
 
-                {/* How did you hear about us? Dropdown */}
                 <Form.Select
                   className="dark-bg mb-3"
                   aria-label="howDidYouHear"
+                  name="howDidYouHear"
+                  value={formData.howDidYouHear}
+                  onChange={handleChange}
                 >
-                  <option disabled>How did you hear about us?</option>
-                  <option value="1">Youtube</option>
-                  <option value="2">Instagram</option>
-                  <option value="3">News Portal</option>
+                  <option disabled value="">
+                    How did you hear about us?
+                  </option>
+                  {platform.map((item) => (
+                    <option key={item.id} value={item.value}>
+                      {item.label}
+                    </option>
+                  ))}
                 </Form.Select>
 
-                {/* Select Interested Categories */}
                 <Form.Group
                   as={Col}
                   className="mb-3"
@@ -126,24 +206,25 @@ const BrandSignup = () => {
                     Select Interested Categories
                   </Form.Label>
                   <Select
+                    options={category}
                     isMulti
-                    name="category"
+                    name="category" // Set a name for the select
                     id="category"
-                    className="basic-multi-select  mb-3"
+                    className="basic-multi-select mb-3"
                     classNamePrefix="select a category"
-                    required
+                    value={category.filter((option) =>
+                      formData.interestedCategories.includes(option.value)
+                    )}
+                    onChange={handleCategoryChange} // Use the handler for changes
                   />
-                  <Form.Control.Feedback type="invalid">
-                    {/* {errors.category} */}
-                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <div className="mt-3 d-grid justify-content-center">
-                  <button type="submit" className="btn-global brand-btn py-2 ">
+                  <Button type="submit" className="btn-global brand-btn py-2">
                     Sign up
-                  </button>
+                  </Button>
                 </div>
-              </Form>
+              </form>
               <div className="text-center my-2">
                 <small className="text-secondary">
                   By signing up, you agree to our
