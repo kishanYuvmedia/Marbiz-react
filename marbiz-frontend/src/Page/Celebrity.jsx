2a76d10610767fccbf095cbdc55180860f31f77b
Anshul Kumar <anshulkm17@gmail.com>
2023-10-24 18:23:41 +0530

    added celeb slider

--- marbiz-frontend/src/Page/Celebrity.jsx
+++ marbiz-frontend/src/Page/Celebrity.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Container, Row } from 'react-bootstrap'
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { BsStopwatch } from "react-icons/bs";
 import { PiShootingStar, PiWallet } from "react-icons/pi";
@@ -9,14 +9,15 @@ import artist_3 from "../Images/artist_3.webp";
 import artist_4 from "../Images/artist_4.webp";
 import CelebBox from '../Components/CelebBox';
 
+import CelebritySlider from '../Components/CelebritySlider';
+
 const Celebrity = () => {
 
-
   return (
-    <div className='new-brand-box'>
+    <div className='new-brand-box' >
 
       {/* Search filters */}
-      <section className='brand-banner position-relative'>
+      <section className='brand-banner position-relative' >
 
         <Container className='py-5'>
           <Row className=''>
@@ -50,32 +51,10 @@ const Celebrity = () => {
             </div>
 
             <div className="col-md-6 col-12">
-              <div className="text-center p-3">
-                <img
-                  src={artist_3}
-                  alt="artist_3"
-                  className="rounded-3 img-fluid"
-                />
-                {/* <div className="new-slider owl-carousel owl-loaded owl-drag">
-                  <div className="owl-stage-outer owl-height" style={{ height: "419px" }}><div className="owl-stage" style={{ transition: "all 0.25s ease 0s", width: "2852px", transform: "translate3d(-1247px, 0px, 0px)" }}>
-                    <div className="owl-item cloned" style={{"width: 316.5px; margin-right: 40px;"}}><div className="new-img owl-slide-img"> <video className="banner-video" id="video-3" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/shivaji_satam_b2b.png?tr=q-80" style={{objectFit: "fill"}}>
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/shivaji_satam_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item cloned" style={{ width: "316.5px", marginRight: "40px" }}><div className="new-img owl-slide-img"> <video className="banner-video" id="video-4" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/Sopphie_Chowdhary.jpg?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/sophie_choudhary_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item" style="width: 316.5px; margin-right: 40px;"><div className="new-img owl-slide-img"> <video className="banner-video" id="video-72" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/daisy_shah_b2b.png?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/daisy_shah_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item" style="width: 316.5px; margin-right: 40px;"><div className="new-img owl-slide-img"> <video className="banner-video" id="video-73" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/satyayani_b2b.png?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/shayantani_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item active center" style="width: 316.5px; margin-right: 40px;"><div className="new-img owl-slide-img"> <video className="banner-video" id="video-74" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/shivaji_satam_b2b.png?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/shivaji_satam_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item active" style="width: 316.5px; margin-right: 40px;"><div className="new-img owl-slide-img"> <video className="banner-video" id="video-71" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/Sopphie_Chowdhary.jpg?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/sophie_choudhary_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item cloned" style="width: 316.5px; margin-right: 40px;"><div className="new-img owl-slide-img"> <video className="banner-video" id="video-1" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/daisy_shah_b2b.png?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/daisy_shah_b2b.mp4" type="video/mp4" />
-                    </video></div></div><div className="owl-item cloned" style="width: 316.5px; margin-right: 40px;"><div className="new-img owl-slide-img"> <video className="banner-video" id="video-2" width="100%" loop="" muted="" autoplay="autoplay" playsinline="" poster="https://ik.imagekit.io/j83rchiauw/brand-enquiry/satyayani_b2b.png?tr=q-80" style="object-fit: fill">
-                      <source className="video-sec" src="https://cdn.tring.co.in/img/shayantani_b2b.mp4" type="video/mp4" />
-                    </video></div></div></div></div><div className="owl-nav disabled"><button type="button" role="presentation" className="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" className="owl-next"><span aria-label="Next">›</span></button></div><div className="owl-dots disabled"></div>
-                    </div>*/}
+              <div className="">
+
+                <CelebritySlider />
+
               </div>
             </div>
           </Row>
@@ -192,7 +171,7 @@ const Celebrity = () => {
         </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 
